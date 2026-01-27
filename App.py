0dd5f64e967c037f85b709ffadd9b281ee6938ee
STS-Engineer <administration.sts@avocarbon.com>
2026-01-27 09:03:30 +0100

    Update App.py

--- App.py
+++ App.py
@@ -4848,7 +4848,7 @@ def init_scheduler():
             trigger=CronTrigger(
                 day_of_week='tue',
                 hour=8,  # 17:12 Tunisia = 16:12 UTC
-                minute=10,
+                minute=30,
                 timezone=pytz.UTC
             ),
             id='compliance_check_tue',
